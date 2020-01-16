dbb70c5a2b2fea91f7be2dd1db3991aff0f93d56
Jordan Smith <jordsmithy@gmail.com>
2020-01-16 00:18:12 +0000

    cleaned up dependencies

--- src/Components/ItemComponent.js
+++ src/Components/ItemComponent.js
@@ -3,19 +3,18 @@ import './ItemComponent.css'
 import '../assets/instagram-logo.png'
 import Linkify from 'react-linkify'
 const ItemComponent = (props) => {
-    let loopCount = 0;
     return (
         props.results.map((item) => {
-            loopCount += 1;
             return (
                 <>
                         {/* Post */}
                         {typeof item.item_data.text !== 'undefined' && props.filter === 'posts' | props.filter === 'all' ? 
                             <div className="item-box">
                                 <div className="label">AFF</div>
-                                <img src={props.filter === 'posts' | props.filter === 'all' && typeof item.item_data.image_url !== 'undefined' ? item.item_data.image_url : ""}></img>
-                                <p>{props.filter === 'posts' || props.filter === 'all' ? item.item_data.text : ''}</p>
-                                <a href={props.filter === 'posts' || props.filter === 'all' ? item.item_data.link : ''} target="_blank">{props.filter === 'posts' || props.filter === 'all' ? item.item_data.link_text : ''}</a>
+                                <object data={item.item_data.image_url} type="image/png" codetype="image/png">alt</object>
+                                {/* <img src={typeof item.item_data.image_url !== 'undefined' ? fetch(item.item_data.image_url, {mode: 'no-cors', headers:{credentials: 'omit'}}) : ""}></img> */}
+                                <p>{item.item_data.text}</p>
+                                <a href={item.item_data.link} target="_blank">{item.item_data.link_text}</a>
                             </div>
                         : ""}
                         {/* Twitter */}
@@ -24,10 +23,10 @@ const ItemComponent = (props) => {
                                 <div className="label">
                                     <img src={require("../assets/Twitter_Logo_WhiteOnImage.png")} className="twitter-logo" alt="twitter"></img>
                                 </div>
-                                <Linkify properties={{target: '_blank'}}>
-                                    <p>{typeof item.item_data.tweet !== 'undefined' && props.filter === 'tweets' | props.filter === 'all' ? item.item_data.tweet : ""}</p>
+                                <Linkify>
+                                    <p>{item.item_data.tweet}</p>
                                 </Linkify>
-                                <p>{typeof item.item_data.tweet !== 'undefined' && props.filter === 'tweets' | props.filter === 'all' ? "- @" + item.item_data.user.username : ''}</p>
+                                <p>{"- @" + item.item_data.user.username}</p>
                             </div>
                         : ""}
                         {/* Instagram */}
@@ -36,8 +35,8 @@ const ItemComponent = (props) => {
                                 <div className="label">
                                     <img className="instagram-logo" src={require("../assets/instagram-logo.png")}></img>
                                 </div>
-                                <img src={item.service_name === "Instagram" && props.filter === 'instagram' | props.filter === 'all' ? item.item_data.image.medium : ''}></img>
-                                <p>{item.service_name === "Instagram" && props.filter === 'instagram' | props.filter === 'all' ? item.item_data.caption : ""}</p>
+                                <img src={item.service_name === "Instagram" ? item.item_data.image.medium : ''}></img>
+                                <p>{item.service_name === "Instagram" ? item.item_data.caption : ""}</p>
                                 <p className="hashtag">{item.service_name === "Instagram" ? item.item_data.tags.map(tag => '#' + tag + " ") : ""}</p>
                             </div>
                         : ""}
